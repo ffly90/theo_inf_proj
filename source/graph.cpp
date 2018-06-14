66ca00feb0ed39ed3ddf773ec784d6cff6b5cb4d
user <firefly-serenity@github.com>
2018-06-14 11:26:51 +0200

    corrected include, removed semicolon, added function definition to print the edges

--- source/graph.cpp
+++ source/graph.cpp
@@ -1,4 +1,10 @@
 #include <iostream>
-#include <graph.cpp>
+#include "graph.hpp"
 
-Graph::Graph(std::vector<Edge> edges_):_edges(edges_){};
+Graph::Graph(std::vector<Edge> edges_):_edges(edges_){}
+
+void Graph::printedges(){
+  for (unsigned int i = 0; i < _edges.size(); i++){
+  std::cout << "(" << _edges[i].x << "," << _edges[i].y << ")" << " ";
+  }
+}
