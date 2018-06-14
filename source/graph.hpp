31902080996718ce30cb9899427b3a3cf83663ac
Steffen Walter <firefly-serenity@github.com>
2018-06-14 09:09:34 +0200

    begun to write class graph

--- source/graph.hpp
+++ source/graph.hpp
@@ -1,8 +1,14 @@
 #ifndef GRAPH_HPP
 #define GRAPH_HPP
+#include <vector>
+#include "edge.hpp"
+
 
 class Graph{
-
-}
+public:
+  Graph(std::vector<Edge> edges_);
+private:
+  std::vector<Edge> _edges;
+};
 
 #endif //GRAPH_HPP
