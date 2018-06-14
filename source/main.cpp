a8ff1c9eeaf67559af1d064fc8b5433b92bbb953
Steffen Walter <firefly-serenity@github.com>
2018-06-14 09:10:04 +0200

    put in some sample data

--- source/main.cpp
+++ source/main.cpp
@@ -3,3 +3,10 @@ Projektarbeit von Sidney Kuyateh, Marcel Nowak, Thomas Schäberle und Steffen Wa
 */
 #include "main.hpp"
 #include "graph.hpp"
+
+/*initialize data*/
+int main()
+{
+  Graph sample1({{a,b},{b,c},{c,a},{c,b},{d,c},{e,f},{f,e}});
+  return 0;
+}
