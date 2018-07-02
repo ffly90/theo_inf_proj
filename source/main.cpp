e88f72abc4a1ae59f57d35d061a43b3e2964fdff
sidneyjohn23 <sidneyjohn23@icloud.com>
2018-07-02 15:47:43 +0200

    Sorting algorithm implemented, size reduced

--- source/main.cpp
+++ source/main.cpp
@@ -3,10 +3,10 @@ Projektarbeit von Sidney Kuyateh, Marcel Nowak, Thomas Schäberle und Steffen Wa
 */
 #include <string>
 #include <iostream>
+#include <algorithm>
 #include "main.hpp"
 #include "graph.hpp"
 #define ERROR -1
-//#define DEBUG
 
 void CalculateVertexes(Graph g);
 void PrintVertexList(std::vector<int> vertexlist);
@@ -34,63 +34,31 @@ int main()
 	std::vector<int> vertexB = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31 };
 	std::vector<Edge> edgesB = { { 1,2 },{ 1,3 },{ 2,3 },{ 2,4 },{ 2,5 },{ 2,7 },{ 3,4 },{ 3,6 },{ 3,7 },{ 4,5 },{ 4,8 },{ 4,9 },{ 4,11 },{ 5,6 },{ 5,8 },{ 5,10 },{ 5,11 },{ 6,7 },{ 6,12 },{ 6,13 },{ 6,15 },{ 7,12 },{ 7,14 },{ 7,15 },{ 8,9 },{ 8,16 },{ 8,17 },{ 8,19 },{ 9,10 },{ 9,16 },{ 9,18 },{ 9,19 },{ 10,11 },{ 10,20 },{ 10,21 },{ 10,23 },{ 11,12 },{ 11,20 },{ 11,22 },{ 11,23 },{ 12,13 },{ 12,24 },{ 12,25 },{ 12,27 },{ 13,14 },{ 13,24 },{ 13,26 },{ 13,27 },{ 14,15 },{ 14,28 },{ 14,29 },{ 14,31 },{ 15,28 },{ 15,30 },{ 15,31 },{ 16,17 },{ 17,18 },{ 18,19 },{ 19,20 },{ 20,21 },{ 21,22 },{ 22,23 },{ 23,24 },{ 24,25 },{ 25,26 },{ 26,27 },{ 27,28 },{ 28,29 },{ 29,30 },{ 30,31 } };
 
-
-	Graph graph(vertexB, edgesB); //changed order
-#ifdef DEBUG
-	graph.printvertex();
-	graph.printedges();
-#endif // DEBUG
-	CalculateVertexes(graph);
+	Graph graphB(vertexB, edgesB); //changed order
+	CalculateVertexes(graphB);
 	return 0;
 }
 
 void CalculateVertexes(Graph g)
 {
 	std::vector<int> vertexlist;
-
-	while (true)
+	g.SortVertexesByDegree(); // Complexity: O(n * log(n))
+	g.printvertex();
+	for (int v : g.Vertexes())
 	{
-		int max_vertex = 0, max_vertex_count = 0;
-		for (int v : g.Vertexes())
+		for (Edge e : g.GetEdgesOfVertex(v))
 		{
-			if (g.GetEdgeCount(v) > max_vertex_count && !(ListContains<int>(vertexlist, v)) && g.HasUnanalyzedEdges(v))
-			{
-				max_vertex = v;
-				max_vertex_count = g.GetEdgeCount(v);
-			}
-		}
-		if (max_vertex == 0)
-		{
-			break;
-		}
-#ifdef DEBUG
-		std::cout << "Analyze Vertex: " << max_vertex << std::endl;
-#endif // DEBUG
-		for (Edge e : g.GetEdgesOfVertex(max_vertex))
-		{
-#ifdef DEBUG
-			std::cout << "Edge: (" << e.x << "," << e.y << "," << g.EdgeAnalyzed()[e] << ") of Vertex " << max_vertex << std::endl;
-#endif // DEBUG
 			if (g.EdgeAnalyzed()[e] != true)
 			{
-				if (!(ListContains<int>(vertexlist, max_vertex)))
+				if (!(ListContains<int>(vertexlist, v)))
 				{
-					vertexlist.insert(vertexlist.end(), max_vertex);
+					vertexlist.insert(vertexlist.end(), v);
 				}
-#ifdef DEBUG
-				std::cout << "Analyze Edge: (" << e.x << "," << e.y << ") of Vertex " << max_vertex << std::endl;
-#endif // DEBUG
 				g.SetEdgeAnalyzed(e);
 			}
 		}
-#ifdef DEBUG
-		PrintVertexList(vertexlist);
-#endif // DEBUG
 	}
 	PrintVertexList(vertexlist);
-#ifdef DEBUG
-	g.printedges();
-#endif // DEBUG
 
 }
 
